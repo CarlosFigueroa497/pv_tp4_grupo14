353973c8027ffe6aeb5f755b6c4f0cc92e55662c
Hernan <hernanjairo@gmail.com>
2025-05-20 19:01:05 -0300

    se agrego las funcionalidades a los componentes productlist y productitem

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,12 @@
-function ProductItem() {
+function ProductItem({ producto }) {
   return (
-    <div>
-      <p>Producto individual</p>
+    <div style={{ border: '1px solid gray', marginBottom: '10px', padding: '10px' }}>
+      <p><strong>ID:</strong> {producto.id}</p>
+      <p><strong>Descripción:</strong> {producto.descripcion}</p>
+      <p><strong>Precio Unitario:</strong> ${producto.precioUnitario}</p>
+      <p><strong>Descuento:</strong> {producto.descuento}%</p>
+      <p><strong>Precio con Descuento:</strong> ${producto.precioConDescuento.toFixed(2)}</p>
+      <p><strong>Stock:</strong> {producto.stock}</p>
     </div>
   );
 }
