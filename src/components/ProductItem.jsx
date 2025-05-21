2c4364fb35f2089dfad31b316532be56c070694b
juanicolas619 <juanicolas619@gmail.com>
2025-05-20 21:21:32 -0300

    nuevas mejoras y justificacion a la lista de productos

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,11 +1,11 @@
 function ProductItem({ producto, onEliminar, onEditar }) {
   return (
-    <div style={{ border: '1px solid gray', marginBottom: '10px', padding: '10px' }}>
+    <div className="producto-item">
        <p><strong>ID:</strong> {producto.id}</p>
        <p><strong>Descripción:</strong> {producto.descripcion}</p>
        <p><strong>Precio Unitario:</strong> ${producto.precioUnitario}</p>
        <p><strong>Descuento:</strong> {producto.descuento}%</p>
-       <p><strong>Precio con Descuento:</strong> ${producto.precioConDescuento.toFixed(2)}</p>
+       <p><strong>Precio con Descuento:</strong> <span className="descuento-resaltado">${producto.precioConDescuento.toFixed(2)}</span></p>
        <p><strong>Stock:</strong> {producto.stock}</p>
       <button onClick={() => onEditar(producto)}>Editar</button>
       <button onClick={() => onEliminar(producto.id)}>Eliminar</button>
